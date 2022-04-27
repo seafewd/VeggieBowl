63a50931c7041912b55a381f5d3895d45aab25e7
alex <alexander.korpas@protonmail.com>
2022-04-27 22:02:57 +0200

    fix?

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,10 +2,9 @@
 
 const mongoose = require('mongoose');
 
-console.log(process.env.DATABASE)
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true }).then(() => {
+// mongoose.connect(process.env.DATABASE, { useNewUrlParser: true }).then(() => {
+mongoose.connect("mongodb+srv://admin:521983d1ce99a69c7d5804c4f3ffc09b4b9441c5039e9b157c8ca0462031b47e@veggiebowl.aut2y.mongodb.net/veggiebowl", { useNewUrlParser: true }).then(() => {
     console.log('Connected to VeggieBowl DB successfully!');
 }).catch((e) => {
     console.log('Error while attempting to connect to VeggieBowl DB');
